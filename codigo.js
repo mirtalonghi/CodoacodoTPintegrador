9ae0e9a63ec680dc15f35673c3eaf8ec2bf4a2df
Mirta Wolfsdorf <mirta.wolfsdorf@gmail.com>
2021-10-21 21:13:27 -0300

    Ejercio TP integrador JS

--- codigo.js
+++ codigo.js
@@ -3,18 +3,19 @@ function calcular(){
     var valorelegido=document.getElementById('categoriasLista').value;
     var cant=document.getElementById("cantidad").value;
     var total;
-   
+    
+    console.log(valorelegido);
     switch(valorelegido){
         case "Estudiante":
             total=250*0.8*cant;
             document.getElementById('totalPagar').innerHTML= "Total a Pagar: $" + total;
         break
         case "Trainee":
-            total=250*0.8*cant;
+            total=250*0.5*cant;
             document.getElementById('totalPagar').innerHTML="Total a Pagar: $" + total;
         break;
         case "Junior":
-            total=250*0.8*cant;
+            total=250*0.15*cant;
         
             document.getElementById('totalPagar').innerHTML="Total a Pagar: $" + total;
         break;
